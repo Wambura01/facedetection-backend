2860fc652c6cc0f9149d23a0a4e7d4b22dde106e
Wambura01 <brianwambura96@gmail.com>
2021-05-12 23:00:49 +0300

    Updating the database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-amorphous-81219",
     user: "postgres",
     password: "smartphon96",
     database: "facedetectiondb",
